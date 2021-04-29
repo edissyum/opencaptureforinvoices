07d9d21287f74fb45bab83e5e3430ab228a125f4
Brich40 <brich.oussama@gmail.com>
2021-04-29 16:23:59 +0200

    Add document type

---
+++ src/frontend/app/settings/document-type/document-tree.ts
@@ -0,0 +1,365 @@
+export const TREE_DATA = [
+  // ---------------------- //
+  {
+    'key':'',
+    'text': "Première demande d'agrément maternel à domicile",
+    'code': '0.1'
+  },
+  {
+    'key':'Cerfa_premiere_demande',
+    'text': "Cerfa n°13394-04 daté et signé daté et signé",
+    'code': '0.1.1'
+  },
+  {
+    'key':'CNI ou  Passeport ou Permis',
+    'text': "Pièce d'identité ou titre de séjour du demandeur (CNI, passeport, permis de conduire)",
+    'code': '0.1.1'
+  },
+  {
+    'key': 'Justificatif_domicile',
+    'text': "Justificatif de domicile",
+    'code': '0.1.1'
+  },
+  // ---------------------- //
+  {
+    'key' :'',
+    'text': "Première demande d'agrément maternel en Maison d'assistantes maternelles (MAM)",
+    'code': '0.2'
+  },
+  {
+    'key':'Cerfa_premiere_demande_en_MAM',
+    'text': "Cerfa n°13394-04 daté et signé",
+    'code': '0.2.1'
+  },
+  { 'key':'CNI ou  Passeport ou Permis',
+    'text': "Pièce d'identité ou titre de séjour du demandeur (CNI, passeport, permis de conduire)",
+    'code': '0.2.1'
+  },
+  { 'key':'Justificatif_domicile',
+    'text': "Justificatif de domicile",
+    'code': '0.2.1'
+  },
+  // ---------------------- //
+  {
+    'key' :'',
+    'text': "1er Renouvellement d'agrément Maternel (à domicile ou en MAM)",
+    'code': '0.3'
+  },
+  {
+    'key':'Cerfa_demande_renouvellement',
+    'text': "Cerfa n°13395-02 daté et signé",
+    'code': '0.3.1'
+  },
+  {
+    'key':'CNI ou  PASSEPORT ou PERMIS',
+    'text': "Pièce d'identité ou titre de séjour du demandeur (CNI, passeport, permis de conduire)",
+    'code': '0.3.1'
+  },
+  {
+    'key':'Justificatif_domicile',
+    'text': "Justificatif de domicile",
+    'code': '0.3.1'
+  },
+  {
+    'key':'Attestation_responsabilité_civile',
+    'text': "Attestation assurance Responsabilité Civile",
+    'code': '0.3.1'
+  },
+  {
+    'key':'Attestation_chauffage',
+    'text': "Attestation entretien chauffage",
+    'code': '0.3.1'
+  },
+  {
+    'key':'Attestation_ramonage',
+    'text': "Attestation entretien ramonage",
+    'code': '0.3.1'
+  },
+  // ---------------------- //
+  {
+    'key' :'',
+    'text': "1er renouvellement d'agrément familial",
+    'code': '0.4'
+  },
+  {
+    'key' :'Cerfa_demande_renouvellement',
+    'text': "Cerfa n°13395-02 daté et signé",
+    'code': '0.4.1'
+  },
+  {
+    'key' :'CNI ou  PASSEPORT ou PERMIS',
+    'text': "Pièce d'identité ou titre de séjour du demandeur (CNI, passeport, permis de conduire)",
+    'code': '0.4.1'
+  },
+  {
+    'key' :'Justificatif_domicile',
+    'text': "Justificatif de domicile",
+    'code': '0.4.1'
+  },
+  {
+    'key' :'Evaluation_pratiques_professionnelles_employeur',
+    'text': "Evaluation des pratiques professionnelles par l'employeur",
+    'code': '0.4.1'
+  },
+  // ---------------------- //
+  {
+    'key' :'',
+    'text': "DIVERS",
+    'code': '0.5'
+  },
+  {
+    'key' :'Courrier_demandeur',
+    'text': "Courrier demandeur ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_demandeur_securite',
+    'text': "Courrier demandeur lié à la sécurité",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'courrier_travaux',
+    'text': "Information travaux terminés ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_arret',
+    'text': "Demande arrêt procédure ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Changement_nom',
+    'text': "Courrier d’information de changement de nom",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Divorce',
+    'text': "Courrier d’information de divorce",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Mariage',
+    'text': "Courrier d’information de mariage",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Arrivee_enfant',
+    'text': "Courrier d’information Arrivée d’un nouvel enfant ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Arrivee_adulte_au_foyer',
+    'text': "Courrier d’information Arrivée d’un adulte au foyer",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_modification',
+    'text': "Courrier de demande de modification",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_extension',
+    'text': "Courrier de Demande d’extension",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_derogation',
+    'text': "Courrier de  Demande de dérogation",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_changement_domicile',
+    'text': "Courrier de Demande de changement de domicile",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_arrivee_departement',
+    'text': "Courrier de Demande d’arrivée",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_au_departement_origine',
+    'text': "Courrier au département d’origine ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_depart_departement',
+    'text': "Courrier de Demande de départ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_arret_activite',
+    'text': "Courrier demande arrêt activité ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_inactivite',
+    'text': "Courrier demande inactivité",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_report_delai',
+    'text': "Demande de report de délai",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_incident',
+    'text': "Courrier relatant l’incident",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_plaignant',
+    'text': "Courrier du plaignant",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_IP',
+    'text': "Signalement information préoccupante",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Demande_arret_procedure_suite_suspension',
+    'text': "Demande arrêt procédure suite suspension",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Reponse_procureur',
+    'text': "Reponse du procureur",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_demande_arret_procedure_suite_CCPD',
+    'text': "Demande arrêt procédure suite CCPD",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_demande_recours',
+    'text': "Demande de recours",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier',
+    'text': "Demande de visite",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_formation_effectuee_avant_2007',
+    'text': "Attestation formation effectuée avant 2007",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_60h_partie1',
+    'text': "Attestation formation 60h 1ère partie",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_60h_partie2',
+    'text': "Attestation formation 60h 2ème partie",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_80h',
+    'text': "Attestation formation 80h",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_240h',
+    'text': "Attestation formation 240h ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_60h',
+    'text': "Attestation formation 60h",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_validation_epreuve_partie1',
+    'text': "Attestation validation épreuve 1 ère partie",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_gestes_premiers_secours',
+    'text': "Attestation gestes de 1 secours",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_participation_conférences',
+    'text': "Attestation participation à une conférence",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_diplome',
+    'text': "Diplôme",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Fiche_liaison',
+    'text': "Fiche de liaison",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Plannings_garde_enfants',
+    'text': "Planning d’accueil",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Courrier_accueil_stagiaire',
+    'text': "Courrier de l’ASSMAT informant de l’accueil d’un stagiaire",
+    'code': '0.5.1'
+  },
+      {
+    'key' :'Courrier_accueil_stagiaire',
+    'text': "Courrier de l’ASSMAT informant de l’accueil d’un stagiaire",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Projet_accueil',
+    'text': "Projet d'accueil",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_securite_accessibilite',
+    'text': "Attestation sécurité et accessibilité ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Attestation_assurances ',
+    'text': "Attestation d’assurance",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'MAM_Plan',
+    'text': "Plan de la MAM",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'MAM_Demandes_de_la_MAM',
+    'text': "Courrier provenant de la MAM ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Documents_relatifs_au_marche_public',
+    'text': "Documents relatifs au marché public ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Cerfa_demande_renouvellement',
+    'text': "Programme de la formation",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Bilan',
+    'text': "Bilan de la formation ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Emargement',
+    'text': "Feuille d’émargement ",
+    'code': '0.5.1'
+  },
+  {
+    'key' :'Resultats',
+    'text': "Liste des résultats CAP AEPE",
+    'code': '0.5.1'
+  },
+];
